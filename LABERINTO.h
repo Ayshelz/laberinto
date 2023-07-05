77927dc58dbbf66bf900c8556da82a32964a96ba
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-05 19:47:47 +0200

    Update LABERINTO.h

--- LABERINTO.h
+++ LABERINTO.h
@@ -7,16 +7,17 @@
 // Bibliotecas estándar de C++ que usaremos
 #include <vector>
 #include <string>
+#include "PIEZA.h"
 
 // Definición de la estructura laberinto
 class Laberynth{
 
   // Atributos accesibles desde fuera de la clase:
   public:
-    Laberynth(const std::vector<std::string>& lab);   // Constructor de la clase a partir de un vector de cadenas de caracteres
+    Laberynth(const std::vector<std::string>&);   // Constructor de la clase a partir de un vector de cadenas de caracteres
     void mostrar() const;                             // Muestra el laberinto
-    bool movimiento (int x, int y, int pos) const;    // Devuelve true si el movimiento es posible y false en caso contrario
-    bool objetivo (int x, int y) const;               // Comprueba si el laberinto está resuelto
+    bool movimiento (const Pieza&) const;    // Devuelve true si el movimiento es posible y false en caso contrario
+    bool objetivo (const Pieza&) const;               // Comprueba si el laberinto está resuelto
    
 
 // Atributos accesibles sólo desde la clase: (matriz de caracteres)
