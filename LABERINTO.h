1bbfb80c2f7d3374122ae032dfcd89853f5a3d0a
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-05 18:43:57 +0200

    Definicion laberinto
    Definición de las cabeceras de la clase laberinto

--- LABERINTO.h
+++ LABERINTO.h
@@ -1,14 +1,27 @@
-// Cabecera con la definición de la estructura del laberinto
+// Cabecera con la definición de la estructura del laberinto y funciones asociadas a él
 
-// Instrucciones para el precompilador
+// Instrucciones para el preprocesador
 #ifndef LABERINTO_
 #define LEBERINTO_
 
-// Bibliotecas estándar de C++
+// Bibliotecas estándar de C++ que usaremos
 #include <vector>
 #include <string>
 
 // Definición de la estructura laberinto
-class laberynth{};
+class Laberynth{
+
+  // Atributos accesibles desde fuera de la clase:
+  public:
+    Laberynth(const std::vector<std::string>& lab);   // Constructor de la clase a partir de un vector de cadenas de caracteres
+    void mostrar() const;                             // Muestra el laberinto
+    bool movimiento (int x, int y, int pos) const;    // Devuelve true si el movimiento es posible y false en caso contrario
+    bool objetivo (int x, int y) const;               // Comprueba si el laberinto está resuelto
+   
+
+// Atributos accesibles sólo desde la clase: (matriz de caracteres)
+  private:
+    std::vector<std::vector<char>> matriz;
+};
 
 #endif
