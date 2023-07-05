23e514e4000db45c7440f3235530314ea90a1b16
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-05 17:45:03 +0200

    LABERINTO.h
    Creación de la cabecera para el laberinto

---
+++ LABERINTO.h
@@ -0,0 +1,14 @@
+// Cabecera con la definición de la estructura del laberinto
+
+// Instrucciones para el precompilador
+#ifndef LABERINTO_
+#define LEBERINTO_
+
+// Bibliotecas estándar de C++
+#include <vector>
+#include <string>
+
+// Definición de la estructura laberinto
+class laberynth{};
+
+#endif
