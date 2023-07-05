796c03de5d2dd96b647df8c2b4a5621291ce28e3
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-05 18:50:40 +0200

    LABERINTO.cpp
    Creación del archivo de implementación asociado a la cabecera LABERINTO.h con las cabeceras de las funciones

---
+++ LABERINTO.cpp
@@ -0,0 +1,13 @@
+#include "LABERINTO.h"
+
+// Implementación del constructor:
+Laberynth::Laberynth(const std::vector<std::string>& lab){}
+
+// Implementación de la función mostrar:
+void Laberynth::mostrar() const {}
+
+// Implementación de la función movimiento:
+bool Laberynth::movimiento(int x, int y, int pos) const{}
+
+// Implementación de la función objetivo:
+bool Laberynth:::objetivo (int x, int y){}
