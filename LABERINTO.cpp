a2c8f9ffc594aab032d14d3af50906189e6e821e
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-05 19:04:04 +0200

    Constructor LABERINTO.cpp
    Implementado el constructor de la clase laberinto

--- LABERINTO.cpp
+++ LABERINTO.cpp
@@ -1,7 +1,15 @@
 #include "LABERINTO.h"
 
 // Implementación del constructor:
-Laberynth::Laberynth(const std::vector<std::string>& lab){}
+Laberynth::Laberynth(const std::vector<std::string>& lab){
+  for (const std::string& fila_entrada : lab){   // Por cada vector en el vector de entrada
+    std::vector<char> fila_laberinto;            // Creamos una nueva fila para el laberinto
+    for (char dato : fila_entrada){              // Introducimos cada caracter en dicha fila
+      fila_laberinto.pushback(dato);             
+    }
+    matriz.push_back(fila_laberinto);             // Introducimos la fila una vez completada en la estructura del laberinto
+  }
+}
 
 // Implementación de la función mostrar:
 void Laberynth::mostrar() const {}
