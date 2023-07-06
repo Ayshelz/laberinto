6c12ba4a5b0b59ed295c3b348376ca76c498f37f
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-06 15:36:10 +0200

    Update PIEZA.h

--- PIEZA.h
+++ PIEZA.h
@@ -10,24 +10,26 @@
 // Bibliotecas del proyecto
 #include "LABERINTO.h"
 
+class Laberynth;
+
 // Definición de la estructura pieza
 class Pieza{
 
   // Atributos accesibles desde fuera de la clase:
   public:
-    Pieza();       
-    bool rotar (const Laberynth& );                   
-    bool mover_arriba (const Laberynth& );
-    bool mover_abajo (const Laberynth& );
-    bool mover_derecha (const Laberynth& );
-    bool mover_izquierda (const Laberynth& );
-    void mostrar() const; 
+    Pieza();
+    bool rotar (const Laberynth& lab);
+    bool mover_arriba (const Laberynth& lab);
+    bool mover_abajo (const Laberynth& lab);
+    bool mover_derecha (const Laberynth& lab);
+    bool mover_izquierda (const Laberynth& lab);
+    void mostrar() const;
     int centroi() const;
     int centroj() const;
     int orientacion() const;
-   
 
-// Atributos accesibles sólo desde la clase: 
+
+// Atributos accesibles sólo desde la clase:
   private:
     int centro_fila;
     int centro_columna;
