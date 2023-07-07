2f40d3f07efbb31cf8427e89396b03c315e424ae
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-07 10:06:49 +0200

    FINAL laberinto.h
    Versión final de la cabecera laberinto.h

--- LABERINTO.h
+++ LABERINTO.h
@@ -13,19 +13,18 @@ using namespace std;
 // Bibliotecas del proyecto
 #include "PIEZA.h"
 
+// Declaramos la clase Pieza para no tener errores de compilación
 class Pieza;
 
-// Definición de la estructura laberinto
+// Definición de la clase laberinto
 class Laberynth{
 
   // Atributos accesibles desde fuera de la clase:
   public:
     Laberynth(const std::vector<std::string>&);       // Constructor de la clase a partir de un vector de cadenas de caracteres
-    void mostrar() const;                             // Muestra el laberinto
-    bool objetivo (const Pieza&) const;               // Devuelve TRUE si el laberinto está resuelto y FALSE en caso contrario. Recibe la pieza.
+    void mostrar() const;                             // Método para mostrar el laberinto por pantalla
+    bool objetivo (const Pieza&) const;               // Devuelve TRUE si el laberinto está resuelto y FALSE en caso contrario. Recibe la pieza por referencia
     bool celda_valida(int, int) const;                // Devuelve TRUE si la casilla está vacía y FALSE si la casilla es un obstáculo o está fuera del laberinto
-    bool casilla_rotada(int, int) const;              // Devuelve TRUE si la casilla ya ha sido rotada y FALSE en caso contrario
-    void guardar_rotacion(int, int);                  // Marca una casilla como rotada
 
 // Atributos accesibles sólo desde la clase:
   private:
