c4564bb289d9a5264fc723a40873eb23a3de854c
Ayshelz <138691899+Ayshelz@users.noreply.github.com>
2023-07-07 09:59:05 +0200

    FINAL pieza.h
    Versión final del archivo de cabecera pieza.h

--- PIEZA.h
+++ PIEZA.h
@@ -10,24 +10,34 @@
 // Bibliotecas del proyecto
 #include "LABERINTO.h"
 
+// Se declara la clase Laberynth para no obtener errores de compilación
 class Laberynth;
 
 // Definición de la estructura pieza
 class Pieza{
 
-  // Atributos accesibles desde fuera de la clase:
+  // Métodos accesibles desde fuera de la clase:
   public:
+    // Constructor de la clase. Crea una pieza horizontal cuyo centro está en las coordenadas (0,1)
     Pieza();
-    bool rotar ( Laberynth& lab);
-    bool mover_arriba (const Laberynth& lab);
-    bool mover_abajo (const Laberynth& lab);
-    bool mover_derecha (const Laberynth& lab);
-    bool mover_izquierda (const Laberynth& lab);
-    void mostrar() const;
-    int centroi() const;
-    int centroj() const;
-    int orientacion() const;
+    // Constructor de la clase a partir de dos coordenadas y una orientación
+    Pieza(int, int, int);
 
+    // Métodos para comprobar si puede realizarse un movimiento.
+    // Reciben un laberinto por referencia y devuelvuen TRUE si se puede realizar el movimiento
+    bool correcto_rotar (const Laberynth& lab) const;
+    bool correcto_arriba (const Laberynth& lab) const;
+    bool correcto_abajo (const Laberynth& lab) const;
+    bool correcto_derecha (const Laberynth& lab) const;
+    bool correcto_izquierda (const Laberynth& lab) const;
+
+    // Métodos consultores de los atributos privados de Pieza
+    int centroi() const;        // Devuelve la fila donde se encuentra el centro de la pieza siendo 0 la primera
+    int centroj() const;        // Devuelve la columna donde se encuentra el centro de la pieza siendo 0 la primera
+    int orientacion() const;    // Devuelve 0 si la pieza está horizontsl y 1 si está vertical
+
+    // Método para mostrar toda la información de la pieza en pantalla
+    void mostrar() const;
 
 // Atributos accesibles sólo desde la clase:
   private:
